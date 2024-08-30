ce0d8cd1280149022a9b6edb838aabbec175d222
Charu Saraswat <128489200+charus-dev@users.noreply.github.com>
2024-08-30 23:59:26 +0530

    Added Key Concepts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Greeting from './Greetings';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+// Use of Arrow function here, can also be written without arrow function 
+const App = () => {
   return (
     <div className="App">
       <Greeting />
